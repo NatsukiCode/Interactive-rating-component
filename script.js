e858d295e9e71f5d594f1fee25a76088f4104e9d
Natsuki Okada <coconana.9042@gmail.com>
2023-05-18 10:14:03 +0900

    Adding second page

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+ function goToPage2() {
+    const page1 = document.getElementById('page1');
+    const page2 = document.getElementById('page2');
+    page1.classList.add('hidden');
+    page2.classList.remove('hidden');
+};
